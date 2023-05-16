1e6aceb06f252032503ae2bb8bbf1b30fa89a0b0
Matej Hrica <mtjhrc@gmail.com>
2023-05-16 21:25:26 +0200

    Add argument parsing using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::{command, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command()]
+struct Args {
+    #[clap(subcommand)]
+    /// The subcommand to run
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+
+    /// Add a new task
+    Add {
+        /// The name of the task
+        name: String,
+    },
+
+    /// List all tasks
+    List,
+
+    /// Mark the task as completed
+    Complete {
+        /// The number of the task (can be obtained from list subcommand)
+        task_number: u32,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
